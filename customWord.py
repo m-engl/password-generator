d940234882928e8d86952b9a1300d46b665c559d
m-engl <m.englart@gmail.com>
2020-03-06 19:51:34 +0100

    Update customWord.py

--- customWord.py
+++ customWord.py
@@ -2,7 +2,7 @@
 # 1 mix case randomly
 # 2 change characters to special chars or numbers that are "lookalikes"
 # 3 or both of the above
-# 4 add special characters and/or numbers in between the word's letters (SPECIFY LENGTH of the whole password)
+# 4 add special characters and/or numbers in between the word's letters
 # 5 mix case and add numbers
 # 6 mix case and add special chars
 # 7 mix case and add numbers and special characters
@@ -10,7 +10,7 @@
 #import
 import random
 import string
-import specialsDict as sd
+import charSet as cs
 
 # read in word
 
@@ -34,8 +34,8 @@ def randomize_case(letter):
 
 
 def find_lookalike(letter):
-    if letter.upper() in sd.specialsDict:
-        letter = (random.choice(sd.specialsDict[letter.upper()]))
+    if letter.upper() in cs.specialsDict:
+        letter = (random.choice(cs.specialsDict[letter.upper()]))
         return letter
     else:
         print('use only latin alphabet letters and no diacritic signs')
@@ -45,20 +45,17 @@ def find_lookalike(letter):
 # 1 - mix case randomly      
         
 wordWithRandomCase = ''.join(randomize_case(letter) for letter in customWord)
-
 print(wordWithRandomCase)
 
 # 2 - letters to lookalike signs
 
 weirdLookingWord = ''.join(find_lookalike(letter) for letter in customWord)
-
 print(weirdLookingWord)
 
 # 3 mix case randomly and change characters to special chars or numbers that are "lookalikes"
 #   with two lookalikes
 
 numberOfSpecialCharacters = 3
-
 # numberOfSpecialCharacters = int(input("""
 # How many letters in your word do you want to become
 # special characters or special character clusters? 
@@ -87,11 +84,55 @@ def mix_chars_and_case(customWord):
 
     # list to string again
     mixedCaseAndSpecials = ''.join(wordAsList)
-
     return mixedCaseAndSpecials
-        
-    
-    # 
-
 
 print(mix_chars_and_case(customWord))
+
+# 4 add special characters and/or numbers in between the word's letters
+# OPTIONS: in the beginning, in the end, randomly within the word
+# OPTIONS: add numbers, specialchars or both
+
+numberOfNewSymbols = 2
+chosenSet = cs.numbers
+
+randomIndex = random.randint(0, len(customWord))
+
+choice = 'end'
+# for later use when choosing where to place chars:
+if choice == 'end':
+    position = 0
+elif choice == 'beginning':
+    position = len(customWord)
+else:
+    position = randomIndex
+
+def add_new_symbols_randomly(customWord, position = randomIndex):
+
+    a = numberOfNewSymbols
+    wordAsList = list(customWord)
+
+    for i in range(a):
+        randomSymbol = random.choice(chosenSet)
+        randomIndex = random.randint(0, len(customWord))
+        wordAsList.insert(randomIndex, randomSymbol)
+    
+    return (''.join(wordAsList))
+
+print(add_new_symbols_randomly(customWord))
+
+
+
+
+def add_new_symbols_randomly(customWord):
+
+    a = numberOfNewSymbols
+    wordAsList = list(customWord)
+
+    for i in range(a):
+        randomSymbol = random.choice(chosenSet)
+        randomIndex = random.randint(0, len(customWord))
+        wordAsList.insert(randomIndex, randomSymbol)
+    
+    return (''.join(wordAsList))
+
+# 6 and 7 can be built on the already existing functions
