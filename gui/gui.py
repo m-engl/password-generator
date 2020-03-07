8cfa8eeec71b1c86c2c19df30fc7f65273dbf86c
m-engl <m.englart@gmail.com>
2020-03-07 22:55:18 +0100

    Frame scheme up

--- gui/gui.py
+++ gui/gui.py
@@ -1,2 +1,12 @@
 from tkinter import *
 
+root = Tk()
+root.title('My Ultimate Password Generator')
+
+upperFrame = Frame(root, height=100, width=630, padx=3, pady=3, highlightbackground="black", highlightthickness=1).grid(row=0, column=0, columnspan=2)
+upperLeftFrame = Frame(root, height=200, width=270, padx=3, pady=3, borderwidth=10, highlightbackground="black", highlightthickness=1).grid(row=1, column=0)
+lowerLeftFrame = Frame(root, height=250, width=270, padx=3, pady=3, highlightbackground="black", highlightthickness=1).grid(row=2, column=0)
+rightFrame = Frame(root, height=450, width=360, padx=3, pady=3, highlightbackground="black", highlightthickness=1).grid(row=1, column=1, rowspan = 2)
+lowerFrame = Frame(root, height=40, width=630, padx=3, pady=3, highlightbackground="black", highlightthickness=1).grid(row=3, column=0, columnspan=2)
+
+root.mainloop()
